419085c3b5246aff351f6af84f2fb04a909c74c0
NguyenDangHoangChuong-CDTH17PMC <56815715+NguyenDangHoangChuong-CDTH17PMC@users.noreply.github.com>
2019-10-21 13:47:40 +0700

    upload

---
+++ NhaTroOBJ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiemtra
+{
+    class NhaTroOBJ
+    {
+        public string mant { get; set; }
+        public int loaihinh { get; set; }
+        public float dientich { get; set; }
+        public float GiaPhong { get; set; }
+        public string Dchi { get; set; }
+        public string Quan { get; set; }
+        public string Mota { get; set; }
+        public string NgDangTin { get; set; }
+        public string NgLienHe { get; set; }
+        public int TT { get; set; }
+
+
+    }
+}
